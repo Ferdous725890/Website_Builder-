5a0eaefdd8f26a23eb3960a309d1e7f3e6081bd8
habibur5313 <habiburmamun313@gmail.com>
2025-03-14 16:14:55 +0600

    validate password, added sweetalert in sign-up page

--- app/(authentication)/sign-up/page.js
+++ app/(authentication)/sign-up/page.js
@@ -6,10 +6,13 @@ import "./signup.css";
 import { FiArrowRight, FiEye } from "react-icons/fi";
 import { FiEyeOff } from "react-icons/fi";
 import { useState } from "react";
+import Swal from "sweetalert2";
+import { useRouter } from "next/navigation";
 const SignUP = () => {
   const [showPass, setShowPass] = useState(false);
-
+  const [error,setError] = useState("")
   const { googleSignIn, createUser } = useAuth();
+  const router = useRouter();
 
   const handleGoogleSignIn = () => {
     googleSignIn();
@@ -20,17 +23,44 @@ const SignUP = () => {
     const form = e.target;
     const email = form.email.value;
     const password = form.password.value;
-    console.table(email, password);
+    // console.table(email, password);
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$#!%*?&])[A-Za-z\d@$#!%*?&]{6,}$/;
+
+    if (!passwordRegex.test(password)) {
+      return setError(
+        "Password atleast 1 lowercase letter 1 uppercase letter 1 number 1 special characters and atleast 6 characters"
+      );
+      
+    } 
     try {
-      createUser(email, password);
+      const res = await createUser(email, password)
+      const data = res.user
+      if(data){
+        Swal.fire({
+          position: "top-center",
+          icon: "success",
+          title: "Sign Up Successfully",
+          showConfirmButton: false,
+          timer: 1500
+        });
+        router.push("/")
+      }
     } catch (err) {
-      console.log(err);
+      Swal.fire({
+        position: "top-center",
+        icon: "error",
+        title: err.message,
+        showConfirmButton: false,
+        timer: 1500
+      });
     }
   };
+  
   return (
     <div className="text-center  mt-10 space-y-4 px-6 sm:max-w-lg sm:mx-auto sm:space-y-10 ">
       <h3 className="text-3xl font-bold leading-10 lg:text-4xl">
-        Let's create your BuildMaster Account.
+        Let&apos;s create your BuildMaster Account.
       </h3>
       <p className="text-gray-700  text-xl">
         Join our community and start building your dream website without any
@@ -69,6 +99,7 @@ const SignUP = () => {
               Password
             </label>
           </div>
+          <p className="text-red-500 font-medium">{error}</p>
           <button
             type="submit"
             className="group border flex items-center justify-center gap-2 border-gray-200 py-3 cursor-pointer text-white bg-black/80 rounded-sm"
