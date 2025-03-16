26ee439f884bf4da3f7f3a093e030dbaad8221da
lukmanxpert <lukmanmiah2004@gmail.com>
2025-03-16 16:02:45 +0600

    add responsive to AnyTypeSite

--- app/Component/AiTools.js
+++ app/Component/AiTools.js
@@ -6,14 +6,12 @@ import StartForFree from "./StartForFree";
 const AiTools = () => {
   return (
     <div>
-      <h1 className=" text-black mt-16 container mx-auto w-11/12">
-        <p className="text-4xl md:text-7xl font-bold  text-center pb-20">
+      <div className=" text-black mt-16 container mx-auto w-11/12">
+        <h1 className="text-4xl md:text-7xl md:mb-14 font-bold  text-center mb-5">
           AI Tools for Faster, <br></br> Smarter Web Creation
-        </p>
-
-        <div className="grid grid-cols-2 ">
+        </h1>
+        <div className="flex flex-col md:flex-row justify-between items-start gap-4">
           <div>
-            {" "}
             <Image
               src={ai}
               width="full"
@@ -21,19 +19,18 @@ const AiTools = () => {
               alt="Picture of the author"
             />
           </div>
-          <div className="text-black  flex flex-col justify-center p-5">
-            <h2 className="text-4xl ">
-              AI Site Planner: From Idea to Wireframes – Fast!
+          <div className="text-black flex flex-col justify-center p-5">
+            <h2 className="text-4xl font-medium">
+              AI Site Planner: From Idea <br /> to Wireframes – Fast!
             </h2>
-            <p className="mt-5">
+            <p className="mb-5 py-2">
               With the power of AI, turn your website vision into a meaningful
               brief, sitemap and wireframe at near-instant speed.
             </p>
-
             <StartForFree></StartForFree>
           </div>
         </div>
-      </h1>
+      </div>
     </div>
   );
 };
--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
-import Image from "next/image";
 import GetStated from "./Component/GetStarted";
 import Category from "./Component/Category";
 import Start from "./Component/Start";
 import AiTools from "./Component/AiTools";
-import AllTools from "./Component/AnytypeSite";
 import AytypeSite from "./Component/AnytypeSite";
 
 export default function Home() {
@@ -11,15 +9,16 @@ export default function Home() {
     <div className="bg-[#FCCAF2] pt-20 bg-gradient-to-t from-[#ece1e9] to-[#FCCAF2] pb-56">
       {/* -----------------banner section--------- */}
       <div className="container mx-auto w-11/12">
-        <div></div>
         <div>
-          <h2 className="text-4xl md:text-7xl font-bold text-black text-center">
-            Build Your Website, <br /> Shape Your Future!
-          </h2>
-          <p className="text-black text-center mt-5">
-            Create, host, and expand your dream website effortlessly <br></br>{" "}
-            with the ultimate WordPress platform.
-          </p>
+          <div>
+            <h2 className="text-4xl md:text-7xl font-bold text-black text-center">
+              Build Your Website, <br /> Shape Your Future!
+            </h2>
+            <p className="text-black text-center mt-5">
+              Create, host, and expand your dream website effortlessly <br></br>{" "}
+              with the ultimate WordPress platform.
+            </p>
+          </div>
           <GetStated></GetStated>
           <Category></Category>
           <Start></Start>
@@ -28,8 +27,6 @@ export default function Home() {
 
           <AytypeSite></AytypeSite>
         </div>
-
-        <div> </div>
       </div>
     </div>
   );
