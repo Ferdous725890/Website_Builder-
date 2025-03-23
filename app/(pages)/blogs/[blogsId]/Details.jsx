e35c286574f9baaef0fd5028e1b6784aaa2f71e9
habibur5313 <habiburmamun313@gmail.com>
2025-03-23 16:14:40 +0600

    details page completed

--- app/(pages)/blogs/[blogsId]/Details.jsx
+++ app/(pages)/blogs/[blogsId]/Details.jsx
@@ -1,11 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Details = ({ blog }) => {
   return (
     <div>
-      <h1 className="text-center text-3xl font-semibold mt-10">
-        {blog.title} details coming soon{" "}
-      </h1>
+      <Image
+        src={blog?.image}
+        layout="responsive"
+        width={100}
+        height={100}
+        alt={blog?.title}
+      />
+      <div className="flex flex-col space-y-3">
+        <h1 className="text-3xl font-semibold md:text-4xl lg:text-5xl md:font-bold mt-10">
+          {blog?.title}
+        </h1>
+        <p className="text-xl sm:font-medium">{blog?.description}</p>
+        <p className="text-2xl font-medium">Steps:</p>
+        {blog?.steps.map((step) => (
+          <div key={step?.step}>
+            <p className="text-xl font-medium">
+              {step?.step}/ {step.title}
+            </p>
+            <p className="text-xl">{step?.description}</p>
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-wrap gap-4 items-center mt-4">
+      <p className="text-2xl font-medium">Resources:</p>
+      {blog?.resources?.length > 0 ? (
+        blog.resources.map((resource, idx) => (
+          <Link
+            href={resource?.link}
+            key={idx}
+            className="mt-1 text-blue-500 hover:text-blue-700 text-xl"
+          >
+            {resource?.name}
+          </Link>
+        ))
+      ) : (
+        <p className="text-2xl font-medium">No resources available.</p>
+      )}
+      </div>
     </div>
   );
 };
--- app/(pages)/blogs/[blogsId]/page.js
+++ app/(pages)/blogs/[blogsId]/page.js
@@ -9,7 +9,9 @@ const page = async ({ params }) => {
 
   return (
     <div className="bg-[#FCCAF2] pt-10 pb-20 bg-gradient-to-t from-[#ece1e9] to-[#FCCAF2] min-h-screen ">
+      <div className="container mx-auto">
       <Details blog={blog}></Details>
+      </div>
     </div>
   );
 };
